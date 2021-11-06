7f1b726b490fbbb535a6c416fe3c6e151f617d7e
Christoph <christoph.parrez@gmail.com>
2021-11-06 15:07:58 +0100

    Added logger to TilService.java

--- src/main/java/com/switchfully/til/service/TilService.java
+++ src/main/java/com/switchfully/til/service/TilService.java
@@ -2,6 +2,8 @@ package com.switchfully.til.service;
 
 import com.switchfully.til.domain.Til;
 import com.switchfully.til.repository.TilRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.UUID;
 @Service
 public class TilService {
     private final TilRepository tilRepository;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public TilService(TilRepository tilRepository) {
         this.tilRepository = tilRepository;
