098bbd1546d9571da4490463b4ee20b0dc763677
Christoph <christoph.parrez@gmail.com>
2021-11-06 14:49:32 +0100

    Created package structure

--- src/main/java/com/switchfully/til/api/TilController.java
+++ src/main/java/com/switchfully/til/api/TilController.java
@@ -1,5 +1,7 @@
-package com.switchfully.til;
+package com.switchfully.til.api;
 
+import com.switchfully.til.domain.Til;
+import com.switchfully.til.service.TilService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
